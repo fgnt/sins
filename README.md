a8bfed96a221ad62b6e3caf89c752d2b5d6bb6fc
Janek Ebbers <ebbers@nt.upb.de>
2020-01-16 15:36:37 +0100

    use download helpers from paderbox

--- README.md
+++ README.md
@@ -29,7 +29,8 @@ If you are using the SINS database please cite the following paper:
 ## Installation
 Install requirements
 ```bash
-$ pip install --user git+https://github.com/fgnt/lazy_dataset.git
+$ pip install --user git+https://github.com/fgnt/lazy_dataset.git@ec06c1e8ff4ccb09420d2d641db8f6d9b1099a4f
+$ pip install --user git+https://github.com/fgnt/paderbox.git@7b3b4e9d00e07664596108f987292b8c78d846b1
 ```
 
 Clone the repo
--- sins/database/download.py
+++ sins/database/download.py
@@ -12,15 +12,16 @@ download living room nodes:
 python3 download.py -db /destination/path/to/db -n 1,2,3,4,6,7,8
 """
 import argparse
-import logging
 import os
-import socket
 import sys
-import tarfile
-import zipfile
-from os import path
-from urllib.request import urlretrieve
+import shutil
+from pathlib import Path
 
+from paderbox.io.download import (
+    download_file_list,
+    download_file,
+    extract_file
+)
 from tqdm import tqdm
 
 RECORDS = {
@@ -39,139 +40,13 @@ RECORDS = {
 }
 
 
-def download_file(remote_file, local_file):
-    """
-    Download single file to local_dir
-
-    Args:
-        remote_file:
-        local_file:
-
-    Returns:
-
-    """
-    local_dir = path.dirname(local_file)
-    if not path.isfile(local_file):
-        def progress_hook(t):
-            """
-            Wraps tqdm instance. Don't forget to close() or __exit__()
-            the tqdm instance once you're done with it (easiest using
-            `with` syntax).
-            """
-
-            last_b = [0]
-
-            def inner(b=1, bsize=1, tsize=None):
-                """
-                b  : int, optional
-                    Number of blocks just transferred [default: 1].
-                bsize  : int, optional
-                    Size of each block (in tqdm units) [default: 1].
-                tsize  : int, optional
-                    Total size (in tqdm units). If [default: None]
-                    remains unchanged.
-                """
-                if tsize is not None:
-                    t.total = tsize
-                t.update((b - last_b[0]) * bsize)
-                last_b[0] = b
-
-            return inner
-
-        tmp_file = path.join(local_dir, 'tmp_file')
-        with tqdm(
-                desc="{0: >25s}".format(
-                    path.splitext(remote_file.split('/')[-1])[0]),
-                file=sys.stdout,
-                unit='B',
-                unit_scale=True,
-                miniters=1,
-                leave=False,
-                ascii=True
-        ) as t:
-
-            urlretrieve(
-                remote_file,
-                filename=tmp_file,
-                reporthook=progress_hook(t),
-                data=None
-            )
-        os.rename(tmp_file, local_file)
-    return local_file
-
-
-def extract_file(local_file):
-    """
-    extract files if local_file zip or tar.gz else do nothing
-
-    Args:
-        local_file:
-
-    Returns:
-
-    """
-
-    local_dir = path.dirname(local_file)
-    if path.isfile(local_file):
-
-        if local_file.endswith('.zip'):
-
-            with zipfile.ZipFile(local_file, "r") as z:
-                # Trick to omit first level folder
-                parts = []
-                for name in z.namelist():
-                    if not name.endswith('/'):
-                        parts.append(name.split('/')[:-1])
-                prefix = path.commonprefix(parts) or ''
-
-                if prefix:
-                    if len(prefix) > 1:
-                        prefix_ = list()
-                        prefix_.append(prefix[0])
-                        prefix = prefix_
-
-                    prefix = '/'.join(prefix) + '/'
-                offset = len(prefix)
-
-                # Start extraction
-                members = z.infolist()
-                for i, member in enumerate(members):
-                    if len(member.filename) > offset:
-                        member.filename = member.filename[offset:]
-                        if not path.isfile(path.join(
-                                local_dir, member.filename)):
-                            try:
-                                z.extract(member=member, path=local_dir)
-                            except KeyboardInterrupt:
-                                # Delete latest file, since most likely it
-                                # was not extracted fully
-                                os.remove(
-                                    path.join(local_dir, member.filename)
-                                )
-
-                                # Quit
-                                sys.exit()
-            os.remove(local_file)
-
-        elif local_file.endswith('.tar.gz'):
-            tar = tarfile.open(local_file, "r:gz")
-            for i, tar_info in enumerate(tar):
-                if not path.isfile(
-                        path.join(local_dir, tar_info.name)):
-                    tar.extract(tar_info, local_dir)
-                tar.members = []
-            tar.close()
-            os.remove(local_file)
-
-
-def download_node(node_id, database_path, logger=None):
+def download_node(node_id, database_path):
     """
     Download data from single node to database_path
 
     Args:
         node_id:
         database_path:
-        logger:
 
     Returns:
 
@@ -185,40 +60,7 @@ def download_node(node_id, database_path, logger=None):
             for j in range(1, 10 + (int(node_id) < 9))
         ] + ['license.pdf'] + (['readme.txt'] if node_id != '1' else [])
     ]
-    os.makedirs(database_path, exist_ok=True)
-
-    # Set socket timeout
-    socket.setdefaulttimeout(120)
-
-    item_progress = tqdm(
-        files, desc="{0: <25s}".format('Download files'),
-        file=sys.stdout, leave=False, ascii=True)
-
-    local_files = list()
-    for remote_file in item_progress:
-        local_files.append(download_file(
-            remote_file, path.join(database_path, path.basename(remote_file))
-        ))
-
-    item_progress = tqdm(local_files,
-                         desc="{0: <25s}".format('Extract files'),
-                         file=sys.stdout,
-                         leave=False,
-                         ascii=True)
-
-    if logger is not None:
-        logger.info('Starting Extraction')
-    for _id, local_file in enumerate(item_progress):
-        if local_file and path.isfile(local_file):
-            if logger is not None:
-                logger.info(
-                    '  {title:<15s} [{item_id:d}/{total:d}] {package:<30s}'
-                    .format(
-                        title='Extract files ',
-                        item_id=_id,
-                        total=len(item_progress),
-                        package=local_file))
-            extract_file(local_file)
+    download_file_list(files, database_path, exist_ok=True)
 
 
 def download(nodes, database_path):
@@ -232,16 +74,13 @@ def download(nodes, database_path):
     Returns:
 
     """
-    logger = logging.getLogger('download')
+    database_path = Path(database_path)
     os.makedirs(database_path, exist_ok=True)
-
-    # Set socket timeout
-    socket.setdefaulttimeout(120)
 
     # Download git repository with annotations and matlab code
     extract_file(download_file(
         'https://github.com/KULeuvenADVISE/SINS_database/archive/master.zip',
-        path.join(database_path, 'SINS_database-master.zip')
+        database_path / 'SINS_database-master.zip'
     ))
 
     # Download node data from zenodo repositories
@@ -250,7 +89,15 @@ def download(nodes, database_path):
         file=sys.stdout, leave=False, ascii=True)
 
     for node_id in item_progress:
-        download_node(node_id, database_path, logger)
+        download_node(node_id, database_path)
+    shutil.move(
+        str(database_path / 'Original' / 'audio'),
+        str(database_path / 'audio')
+    )
+    os.rmdir(str(database_path / 'Original'))
+    for file in (database_path / 'SINS_database-master').iterdir():
+        shutil.move(str(file), str(database_path))
+    os.rmdir(str(database_path / 'SINS_database-master'))
 
 
 if __name__ == '__main__':
